c528a3ec9157744b26300636908089ec99d9cf2f
Guilherme Righetto <guilhermerighetto@hotmail.com>
2016-12-15 16:08:22 -0200

    Change args

--- ExtractFeatWeight.py
+++ ExtractFeatWeight.py
@@ -9,28 +9,63 @@ from keras import backend as K
 
 import theano
 import random
+import time
+import sys
+
+#OUTPUT FORMAT FILES:
+#   Diss = True: feat_1 feat_2 feat_3 ... feat_n class_1
+#                feat_1 feat_2 feat_3 ... feat_n class_2
+#                                                      .
+#                                                      .
+#                                                      .                                         
+#                feat_1 feat_2 feat_3 ... feat_n class_n
+
+#   Diss = False (SVM FORMAT): class_1 1:feat_1 2:feat_2 3:feat_3 ... n:feat_n
+#                              class_2 1:feat_1 2:feat_2 3:feat_3 ... n:feat_n
+#                                                                            .
+#                                                                            .
+#                                                                            .                                        
+#                              class_n 1:feat_1 2:feat_2 3:feat_3 ... n:feat_n 
+
+try:
+    if(sys.argv[1] != None):
+        path_images = sys.argv[1]
+        path_weights = sys.argv[2]
+        path_output_file_train = sys.argv[7]
+        path_output_file_test = sys.argv[8]
+        images_format = '.bmp'
+        nb_img_class = sys.argv[3]
+        train = 60
+        test = 40
+        classes = sys.argv[4]
+        nb_block = sys.argv[5]
+        block_height = sys.argv[6]
+        block_width = sys.argv[6]
+        preName = "CF"
+        sep="_"
+        channels_img = 1
+except:
+    path_images = "../FolderImages"
+    path_weights = "../file_weights.hdf5"
+    path_output_file_train = "../file.train"
+    path_output_file_test = "../file.test"
+    images_format = '.bmp'
+    nb_img_class = 3
+    train = 60
+    test = 40
+    classes = 50
+    nb_block = 105
+    block_height = 128
+    block_width = 128
+    preName = "CF"
+    sep="_"
+    channels_img = 1
 
 
-#pathImages = '/home/guilherme/Documentos/Bases/CVL_309_100_128/'
-pathImages = '/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/BFL_315_100_128/'
-imagesFormat = '.bmp'
-numImgClass = 3
-train = 60
-test = 40
-classes = 230
-epoch = 30
-batchSize = 48
-numblock = 9
-blockHeight = 128
-blockWidth = 128
-preName = "CF"
-sep="_"
-channelsImg = 1
-
-def buildModelCVL():
+def buildModel():
     model = Sequential()
 
-    model.add(Convolution2D(32, 3, 3, activation='relu', name='conv1_1',input_shape=(channelsImg, blockHeight, blockWidth), border_mode='same'))
+    model.add(Convolution2D(32, 3, 3, activation='relu', name='conv1_1',input_shape=(channels_img, block_height, block_width), border_mode='same'))
 
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
@@ -47,10 +82,10 @@ def buildModelCVL():
 
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 
-    model.add(Convolution2D(128, 3, 3, border_mode='same'))
+    model.add(Convolution2D(128, 2, 2, border_mode='same'))
     model.add(Activation('relu'))
 
-    model.add(Convolution2D(128, 3, 3, border_mode='same'))
+    model.add(Convolution2D(128, 2, 2, border_mode='same'))
     model.add(Activation('relu'))
 
     model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
@@ -65,102 +100,41 @@ def buildModelCVL():
     model.add(Dense(classes))
     model.add(Activation("softmax"))
 
-
-    model.load_weights("/home/guilherme/Documentos/CNN/Script/BFL_115_100_128_2048.hdf5")
-    #model.load_weights("/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/Pesos/QUWI/QUWI_115_128_2048_Ingles1.hdf5")
-
-    return model
-
-def buildModel():
-    model = Sequential()
-
-    model.add(Convolution2D(32, 3, 3, activation='relu', name='conv1_1',input_shape=(channelsImg, blockHeight, blockWidth), border_mode='same'))
-
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-    model.add(Convolution2D(32, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-    model.add(Convolution2D(128, 2, 2, border_mode='same'))
-    model.add(Activation('relu'))
-
-    model.add(Convolution2D(128, 2, 2, border_mode='same'))
-    model.add(Activation('relu'))
-
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-    model.add(Flatten())
-
-    model.add(Dense(4096))
-    model.add(Activation("relu"))
-
-    model.add(Dropout(0.5))
-
-    model.add(Dense(classes))
-    model.add(Activation("softmax"))
-
-    model.load_weights("/home/guilherme/Documentos/CNN/Script/CVL_115_100_128_4096.hdf5")
+    model.load_weights(path_weights)
 
     return model
 
 
 if __name__ == "__main__":
 
+    #Flag to calculate the dissimilarity approach (github.com/guirighetto/Dissimilaridade)
     diss = False
    
-    numSamplesTrain = float(numImgClass*(float(train)/100))
+    numSamplesTrain = float(nb_img_class*(float(train)/100))
     numSamplesTrain = round(numSamplesTrain)
 
-    model = buildModelCVL()
+    model = buildModel()
 
     model.compile(loss="categorical_crossentropy", optimizer="Adadelta",metrics=['accuracy'])
 
+    start_time = time.time()
+
     get_feature = K.function([model.layers[0].input],[model.layers[16].output])
 
-    for c in range(116,classes+1):
-        #if(c == 194):
-        #    c+=1
-        for s in range(1,numImgClass+1):
-
+    for c in range(1,classes+1):
+        for s in range(1,nb_img_class+1):
             if(s == 1 or s == 2):
-                folderTrainTest = 'Treino/'
+                folderTrainTest = 'Train/'
             elif(s == 3):
-                folderTrainTest = 'Teste/'
-        #    if(s == 1):
-        #        folderTrainTest = 'Arabe1/'
-        #    elif(s == 2):
-        #        folderTrainTest = 'Arabe2/'
-        #    elif(s == 3):
-        #        folderTrainTest = 'Ingles1/'
-        #    elif(s == 4):
-        #        folderTrainTest = 'Ingles2/'
-            #elif(s == 5):
-            #    folderTrainTest = 'Alemao1/'
+                folderTrainTest = 'Test/'
             else:
                 folderTrainTest = None
 
-            for b in range(1,numblock+1):
-                #if(folderTrainTest == 'Arabe1/' or folderTrainTest == 'Arabe2/' or folderTrainTest == 'Ingles1/' or folderTrainTest == 'Ingles2/'): #or folderTrainTest == 'Alemao1/'):
-                #    nameImg = preName + str(c).zfill(6) + sep +'0'+str(s) + sep + str(b)
-                #    folderClass = preName + str(c).zfill(6) + '/'
-                #    fullPathImg = pathImages + folderClass + folderTrainTest + nameImg + imagesFormat
-                #    image = plt.imread(fullPathImg)
-                #    image = image[np.newaxis]
-
-                if(folderTrainTest == 'Treino/' or folderTrainTest == 'Teste/'):     
+            for b in range(1,nb_block+1):
+                if(folderTrainTest == 'Train/' or folderTrainTest == 'Test/'):     
                     nameImg = preName + str(c).zfill(5) + sep + str(s) + sep + str(b)
                     folderClass = preName + str(c).zfill(5) + '/'
-                    fullPathImg = pathImages + folderClass + folderTrainTest + nameImg + imagesFormat
+                    fullPathImg = path_images + folderClass + folderTrainTest + nameImg + images_format
                     image = plt.imread(fullPathImg)
                     image = image[np.newaxis]
 
@@ -171,13 +145,9 @@ if __name__ == "__main__":
                     dataTrain = np.array(dataTrain)
                     dataTrain = dataTrain.astype("float32")/255
 
-                #    print(dataTrain)
-
-
                 if(diss == False):
-                    if(folderTrainTest == 'Treino/'):
-                    #if(folderTrainTest == 'Arabe1/' or folderTrainTest == 'Ingles2/' or folderTrainTest == 'Arabe2/'):# or folderTrainTest == 'Ingles4/'): #or folderTrainTest == 'Arabe1/'): #or folderTrainTest == 'Arabe2/'):
-                        fi = open('/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/115_Outro/BFL/BFL_115_128_2048_Outro.Train','a')
+                    if(folderTrainTest == 'Train/'):
+                        fi = open(path_output_file_train,'a')
                         k=1
                         fi.write(str(c))
                         for i in get_feature([dataTrain])[0][0]:
@@ -187,9 +157,8 @@ if __name__ == "__main__":
                         fi.write('\n')
                         k=1
                         fi.close()
-                    elif(folderTrainTest == 'Teste/'):
-                    #elif(folderTrainTest == 'Ingles1/'):
-                        fj = open('/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/115_Outro/BFL/BFL_115_128_2048_Outro.Test','a')
+                    elif(folderTrainTest == 'Test/'):
+                        fj = open(path_output_file_test,'a')
                         j=1
                         fj.write(str(c))
                         for i in get_feature([dataTrain])[0][0]:
@@ -200,20 +169,20 @@ if __name__ == "__main__":
                         j=1
                         fj.close()
                 elif(diss == True):
-                    if(folderTrainTest == 'Ingles1/' or folderTrainTest == 'Ingles2/' or folderTrainTest == 'Ingles4/' or folderTrainTest == 'Ingles3/'):
-                    #if(folderTrainTest == 'Treino/'):
-                        fi = open('/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/Restante/CVL/4_1_Alemao1_Full/CVL_309_128_2048_Diss_Alemao1.Train','a')
+                    if(folderTrainTest == 'Train/'):
+                        fi = open(path_output_file_train,'a')
                         for i in get_feature([dataTrain])[0][0]:
                             fi.write(str(i)+' ')
                         fi.write(str(c))                            
                         fi.write('\n')
                         fi.close()
-                    elif(folderTrainTest == 'Alemao1/'):
-                    #elif(folderTrainTest == 'Teste/'):
-                        fj = open('/media/guilherme/1e8ee6ef-1ae9-40f8-920c-939d24a7a626/guilherme/Bases/Restante/CVL/4_1_Alemao1_Full/CVL_309_128_2048_Diss_Alemao1.Test','a')
+                    elif(folderTrainTest == 'Test/'):
+                        fj = open(path_output_file_test,'a')
                         for i in get_feature([dataTrain])[0][0]:
                             fj.write(str(i)+' ')
                         fj.write(str(c))
                         fj.write('\n')
                         j=1
-                        fj.close()   
+                        fj.close()
+
+    print("Done. Time: ",(time.time() - start_time))
